969d124f89c8063360f882972c16ba06e61742f3
kurilenkoe <kurilenko.egor07@gmail.com>
2025-10-16 23:44:29 +0300

    Add files via upload

---
+++ dzlab5.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+#define M_PI 3.14
+
+int main() {
+    double x = 3.251;
+    double y = 0.325;
+    double z = 0.0000466;
+
+    double c = pow(2, pow(y, x)) + pow(pow(3, x), y) - (y * (atan(z) - M_PI / 6.0)) / (fabs(x) + 1.0 / (y * y + 1.0));
+
+    printf("x = %.6f\n", x);
+    printf("y = %.6f\n", y);
+    printf("z = %.10f\n", z);
+    printf("c = %.10f\n", c);
+
+    return 0;
+}
